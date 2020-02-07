1df94e90b7243b39714edcb75c81ea36416051be
diepinto30 <diepinto30@gmail.com>
2020-02-07 15:28:43 -0500

    add project

---
+++ src/main/java/com/TTEnriching/TTEnriching/TtEnrichingApplication.java
@@ -0,0 +1,13 @@
+package com.TTEnriching.TTEnriching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtEnrichingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtEnrichingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TTEnriching/TTEnriching/TtEnrichingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TTEnriching.TTEnriching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtEnrichingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
